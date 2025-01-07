fc89596fdfe9c91f9de8043e010a6d8b40966765
dhakarRaghu <rdhakad2002@gmail.com>
2025-01-07 18:44:17 +0530

    adding payment methods

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -33,7 +33,7 @@ app.post("/hdfcWebhook", async (req, res) => {
             db.onRampTransaction.updateMany({
                 where: {
                     token: paymentInformation.token
-                },
+                }, 
                 data: {
                     status: "Success",
                 }
